d8dce6e3959015c3f8f71e6d99b6040c2490e187
alex <perm.oas@gmail.com>
2025-02-24 12:48:32 +0300

    my dev

--- Manip/rrt-time.h
+++ Manip/rrt-time.h
@@ -10,6 +10,9 @@
 #include <PlanningSubroutines/ConfigurationProblem.h>
 #include "timedPath.h"
 
+#include <nanoflann.hpp>
+#include <Eigen/Core>
+
 struct Node{
   arr q;
   arr x;
@@ -38,6 +41,113 @@ struct Node{
     }
     cost = newCost;
   }
+};
+
+typedef Eigen::Matrix<double, 6, 1> VertexCoordType;
+
+struct Vertex{
+  arr q;
+  
+  VertexCoordType coords; 
+  std::pair<double, double> safe_interval;
+  double cost = -1;
+
+  Vertex *parent = nullptr;
+  std::vector<Vertex*> children{};
+
+  double arrival_time = -1;
+  double before_rewiring_arrival_time = -1;
+  double departure_from_parent_time = -1;
+  int tree_id = -1;
+
+  Vertex(arr _q, const std::pair<double, double> &_safe_interval): 
+  q(_q), safe_interval(_safe_interval) {
+    for (int i = 0; i < q.N; i++) {
+      coords(i) = q(i);
+  }
+  };
+
+  void addChild(Vertex* v){
+    children.push_back(v);
+  }
+
+  void print() const{
+    std::cout << "t: [" << safe_interval.first << " " << safe_interval.second <<"] \n";
+    std::cout << "q: " << q << std::endl;
+  }
+
+  void updateCost(const double newCost){
+    const double diff = cost - newCost;
+
+    for (auto c: children){
+      c->updateCost(c->cost - diff);
+    }
+    cost = newCost;
+  }
+};
+
+#define TREE_DIMENSIONALITY 6
+typedef nanoflann::KDTreeSingleIndexDynamicAdaptor<nanoflann::L2_Simple_Adaptor<double, Tree_nf>, Tree_nf, TREE_DIMENSIONALITY> KdTree;
+
+struct Tree_nf: GLDrawer
+{
+    Tree_nf(): tree_name(""), tree_idx(-1), kd_tree(TREE_DIMENSIONALITY, *this, nanoflann::KDTreeSingleIndexAdaptorParams(25)) {};;
+    Tree_nf(const std::string &tree_name_, size_t tree_idx_) : tree_name(tree_name_), tree_idx(tree_idx_),
+    kd_tree(TREE_DIMENSIONALITY, *this, nanoflann::KDTreeSingleIndexAdaptorParams(25)) {};;
+    ~Tree_nf(){};                                     // destructor
+    Tree_nf(const Tree_nf &other) = delete;            // copy constructor
+    Tree_nf(Tree_nf &&other) = default;                // move constructor
+    Tree_nf &operator=(const Tree_nf &other) = delete; // copy assignment
+    Tree_nf &operator=(Tree_nf &&other) = default;     // move assignment
+
+    std::string tree_name;
+    size_t tree_idx;
+
+    std::vector<Vertex> array_of_vertices;
+    KdTree kd_tree;
+
+    Vertex &getNearestState(const Vertex q){
+      const size_t num_results{1};
+      size_t q_near_idx{0};
+      double out_dist_sqr{0};
+      nanoflann::KNNResultSet<double> result_set(num_results);
+      result_set.init(&q_near_idx, &out_dist_sqr);
+      
+      Eigen::Matrix<double, Eigen::Dynamic, 1> eigenVec(q.q.N);
+    for (int i = 0; i < q.q.N; i++) {
+        eigenVec(i) = q.q(i);
+    }
+      this->kd_tree.findNeighbors(result_set, eigenVec.data(), {0});
+      return array_of_vertices[q_near_idx];
+  }
+
+    void add_vertex(Vertex q_new, Vertex *q_parent, double departure_time, double arrival_time)
+    {
+      q_new.tree_id = this->tree_idx;
+      q_new.parent = q_parent;
+      q_new.arrival_time = arrival_time;
+      q_new.departure_from_parent_time = departure_time;
+      this->array_of_vertices.push_back(q_new);
+  
+      size_t N{array_of_vertices.size() - 1};
+  
+      // this->array_of_vertices.back().ID_in_array = N;
+      this->kd_tree.addPoints(N, N);
+  
+      if (q_parent != nullptr)
+      {
+          q_parent->children.push_back(&(this->array_of_vertices.back()));
+      }
+  }
+    void delete_vertex(int vertex_id)
+    {
+      this->kd_tree.removePoint(vertex_id);
+  }
+
+    template <class BBOX>
+    bool kdtree_get_bbox(BBOX & /* bb */) const { return false; } 
+    inline size_t kdtree_get_point_count() const { return array_of_vertices.size(); }
+    inline double kdtree_get_pt(const size_t idx, const size_t dim) const { return array_of_vertices[idx].q(dim); }  // у них индексация через круглые скобки
 };
 
 
@@ -194,6 +304,7 @@ enum class SamplingType{
 };
 
 using TimedGoalSampler = std::function<void (const double, arr&)>;
+
 struct PathFinder_RRT_Time{
   TimedConfigurationProblem &TP;
 
@@ -288,6 +399,150 @@ struct PathFinder_RRT_Time{
     path.resize(0, leafNode1->q.N);
     for (Node* n: np){
       time.append(n->t);
+      path.append(n->q);
+    }
+
+    return TimedPath(path, time);
+  }
+};
+
+
+struct PathFinder_SIRRT_Time{
+  TimedConfigurationProblem &TP;
+
+  Tree_nf *start_tree;
+  Tree_nf *goal_tree;
+  Tree_nf *current_tree;
+  Tree_nf *other_tree;
+  Vertex *root_node = nullptr;
+
+  double t_start;
+  double t_max;
+  int n_checks = 150;
+  double dt = (t_max - t_start) / n_checks;
+
+  std::vector<std::pair<double, double>> get_safe_intervals(const arr &q){
+    std::vector<std::pair<double, double>> safeIntervals;
+    bool isSafe = false;
+    double intervalStart = t_start;
+
+    for (double t = t_start; t <= t_max; t += dt) {
+        bool currentState = TP.query(q, t)->isFeasible;
+
+        if (currentState && !isSafe) {
+            intervalStart = t;
+            isSafe = true;
+        } else if (!currentState && isSafe) {
+            safeIntervals.push_back({intervalStart, t});
+            isSafe = false;
+        }
+    }
+    if (isSafe) {
+        safeIntervals.push_back({intervalStart, t_max});
+    }
+
+    return safeIntervals;
+  }
+
+  double vmax = .1;
+  double goal_bias = 0.4;
+
+
+  uint maxIter = 1000;
+  bool verbose = false;
+  bool disp = false;
+
+  double tol = 1e-3;
+  bool connect = true;
+  bool optimize = false;
+  uint conv_iter = 500;
+
+  std::vector<bool> periodicDimensions;
+
+  double edge_checking_time_us{0.};
+  double nn_time_us{0.};
+
+  FrameL prePlannedFrames;
+  uint tPrePlanned;
+
+  PathFinder_SIRRT_Time(TimedConfigurationProblem &_TP) :TP(_TP){
+    delta_buffer = arr(TP.C.getJointState().N);
+    periodicDimensions = std::vector<bool>(TP.C.getJointState().N, false);
+
+    for (auto *j: TP.C.activeJoints){
+      if (j->type == rai::JT_hingeX || j->type == rai::JT_hingeY|| j->type == rai::JT_hingeZ){
+        periodicDimensions[j->qIndex] = true;
+      }
+    }
+  };
+
+  arr projectToManifold(const arr &q, const double t){
+    if (prePlannedFrames.N == 0){
+      return q;
+    }
+    if (t > tPrePlanned){
+      return q;
+    }
+      TP.query(q, t);
+    arr qNew = TP.C.getJointState();
+    if (false){
+      TP.C.setJointState(q);
+      TP.C.watch(true);
+  
+      TP.C.setJointState(qNew);
+      TP.C.watch(true);
+    }
+    return qNew * 1.;
+  };
+
+  TimedPath plan(const arr &q0, const double t0, const arr &qT, double tGoalLowerBound=0, double tGoalUpperBound=-1);
+  TimedPath plan(const arr &q0, const double t0, const TimedGoalSampler gs, double tGoalLowerBound=0, double tGoalUpperBound=-1);
+
+  Vertex *get_nearest_node(const VertexCoordType &coords);
+  std::vector<std::pair<Vertex *, int>> get_nearest_node_by_radius(VertexCoordType &coords, double raduis, Tree_nf *tree);
+
+  arr delta_buffer;
+  double q_metric(const arr& d) const;
+  double distance(const Vertex &n1, const Vertex &n2);
+
+  Vertex* extend(Tree* tree, const Vertex &goal, const bool connect = false);
+
+  arr getDelta(const arr &p1, const arr &p2);
+  Vertex* steer(const Vertex &start, const Vertex &goal, bool reverse);
+
+  TimedPath extractPath(Tree* t1, Tree* t2, Vertex* leafNode1, Vertex* leafNode2){
+
+    std::vector<Vertex*> np;
+    {
+      Vertex* n = leafNode1;
+      while(!!n->parent){
+        np.push_back(n);
+        n = n->parent;
+      }
+      np.push_back(n);
+
+      std::reverse(np.begin(), np.end());
+    }
+
+    {
+      Vertex* n = leafNode2;
+      while(n->parent){
+        np.push_back(n);
+        n = n->parent;
+      }
+      np.push_back(n);
+    }
+
+    if (t1->reverse){
+      std::reverse(np.begin(), np.end());
+    }
+
+    arr path;
+    arr time;
+
+    path.resize(0, leafNode1->q.N);
+    for (Vertex* n: np){
+      time.append(n->arrival_time);
       path.append(n->q);
     }
 
